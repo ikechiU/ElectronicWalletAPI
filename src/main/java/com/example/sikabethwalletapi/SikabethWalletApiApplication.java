0d94b390e9f0880341737b5b3467dc007155d05b
Ikechi Ucheagwu <ikechi@hotmail.com>
2023-02-23 20:05:35 +0100

    Dependencies added

---
+++ src/main/java/com/example/sikabethwalletapi/SikabethWalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sikabethwalletapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SikabethWalletApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SikabethWalletApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sikabethwalletapi/SikabethWalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sikabethwalletapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SikabethWalletApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
